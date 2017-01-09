54129a50cb247946b5442b992f284120ca7f2a5d
cjredmond <connor.redmond@gmail.com>
2017-01-09 17:06:47 -0500

    trying to add resume pdf

--- info/views.py
+++ info/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class ResumeView(TemplateView):
+    template_name = "resume.html"
+# def resume_view(request):
+#     response = HttpResponse(content_type="application/pdf")
+#     response['Content-Disposition'] = 'attachment; filename="info/pdf/CJRresumeupdate.docx.pdf"'
+#
+#     p = canvas.Canvas(response)
+#
+#     p.showPage()
+#     p.save()
+#     return response
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,10 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from info.views import IndexView
+from info.views import IndexView, ResumeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name='index_view'),
+    url(r'^resume/$', ResumeView.as_view(), name='resume_view')
 ]
